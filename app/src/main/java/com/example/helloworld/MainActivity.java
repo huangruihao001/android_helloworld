68c4fa9a9b11cef1ec9cf11de9eb12e2dc14ed7d
hrh <1250908116@qq.com>
2019-09-15 17:00:40 +0800

    20190915

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -7,12 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.example.helloworld.listview.ListViewActivity;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mBtnTextView;
     private Button mBtnButton;
     private Button mBtnEditText;
     private Button mBtnRadioButton;
+    private Button mBtnCheckBox;
+    private Button mBtnImageView;
+    private Button mBtnListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,9 @@ public class MainActivity extends AppCompatActivity {
         mBtnButton = findViewById(R.id.btn_button);
         mBtnEditText = findViewById(R.id.btn_edittext);
         mBtnRadioButton = findViewById(R.id.btn_radiobutton);
+        mBtnCheckBox = findViewById(R.id.btn_checkbox);
+        mBtnImageView = findViewById(R.id.btn_imageview);
+        mBtnListView = findViewById(R.id.btn_listview);
 
         setListeners();
     }
@@ -33,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
         mBtnTextView.setOnClickListener(onClick);
         mBtnEditText.setOnClickListener(onClick);
         mBtnRadioButton.setOnClickListener(onClick);
+        mBtnCheckBox.setOnClickListener(onClick);
+        mBtnImageView.setOnClickListener(onClick);
     }
 
     private class OnClick implements View.OnClickListener{
@@ -56,6 +66,18 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.btn_radiobutton:
                     //跳转到RadioButton演示界面
                     intent = new Intent(MainActivity.this, RadioButtonActivity.class);
+                    break;
+                case R.id.btn_checkbox:
+                    //跳转到CheckBox演示界面
+                    intent = new Intent(MainActivity.this, CheckBoxActivity.class);
+                    break;
+                case R.id.btn_imageview:
+                    //跳转到ImageView演示界面
+                    intent = new Intent(MainActivity.this, ImageViewActivity.class);
+                    break;
+                case R.id.btn_listview:
+                    //跳转到ListView演示界面
+                    intent = new Intent(MainActivity.this, ListViewActivity.class);
                     break;
             }
             startActivity(intent);
